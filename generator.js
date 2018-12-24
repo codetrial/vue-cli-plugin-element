f5b85a04a0eb0d3d7d1da93fe7c3aa88378c6784
felixpy <felixpy.1993@gmail.com>
2018-12-24 22:13:38 +0800

    chore: update coding styles

--- generator.js
+++ generator.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const removeFiles = (files = []) => {
   files.forEach(path => {
     if (fs.existsSync(path)) {
-      fs.unlinkSync(path);
+      fs.unlinkSync(path)
     }
   })
 }
@@ -12,7 +12,7 @@ module.exports = (api, options, rootOptions) => {
   api.extendPackage({
     dependencies: {
       'element-ui': '^2.4.11'
-    },
+    }
   })
 
   api.injectImports(api.entryFile, `import './registerElementUI'`)
@@ -21,7 +21,8 @@ module.exports = (api, options, rootOptions) => {
 
   api.onCreateComplete(() => {
     removeFiles([
-      api.resolve('src/components/HelloWorld.vue')
+      api.resolve('src/components/HelloWorld.vue'),
+      api.resolve('src/store.js')
     ])
   })
 }
